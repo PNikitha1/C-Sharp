678b9c809154bc6abdd4a8504b49aaf650c387b6
PALLAPOTHULA NIKITHA <146755096+PNikitha1@users.noreply.github.com>
2025-11-10 22:24:40 +0530

    Add files via upload

---
+++ AccessModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    internal class Program : HelloWorld.AccessModifiers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Program p = new Program();
+            p.Test1();
+            p.Test5();
+            p.Test3();
+            
+        }
+    }
+}
